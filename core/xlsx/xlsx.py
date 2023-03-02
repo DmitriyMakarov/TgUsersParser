4f80eefaa3db280b672a13e67eda22a2d07d7c14
dmakarov <i@dmitrijmakarov.ru>
2023-03-02 16:18:56 +0300

    отлажено формирование отчета в xls

--- core/xlsx/xlsx.py
+++ core/xlsx/xlsx.py
@@ -1,29 +1,21 @@
 import xlsxwriter
-
+import os
 
 headers = {
     'id': 'id', 'bot': 'bot', 'premium': 'premium', 'access_hash': 'access_hash', 'first_name': 'first_name', 'last_name': 'last_name',
                 'username': 'username', 'phone': 'phone', 'status': 'status',
 }
 
-users = [
-    {'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player1', 'bank': 0.06},
-    {'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player2', 'bank': 4.0},
-    {'dailyWinners': 1, 'dailyFree': 2, 'user': 'Player3', 'bank': 3.1},
-    {'dailyWinners': 3, 'dailyFree': 2, 'user': 'Player4', 'bank': 0.32}
-]
 
-def do_xlsx(file_path: str, headers: dict, items: list):
-    with xlsxwriter.Workbook(file_path) as workbook:
+def do(chat_id, data_time, group_name, users):
+    files_path = '../../res/db/files/'
+    if os.path.exists(files_path + chat_id) is not True:
+        os.mkdir(files_path + chat_id)
+    file_name = f'../../res/db/files/{chat_id}/{group_name}_{data_time}.xls'
+    with xlsxwriter.Workbook(file_name) as workbook:
         worksheet = workbook.add_worksheet()
         worksheet.write_row(row=0, col=0, data=headers.values())
-        header_keys = list(headers.keys())
-        for index, item in enumerate(items):
-            row = map(lambda field_id: item.get(field_id, ''), header_keys)
+        header_key = list(headers.keys())
+        for index, user in enumerate(users.values()):
+            row = map(lambda field_id: user.get(field_id, ''), header_key)
             worksheet.write_row(row=index + 1, col=0, data=row)
-
-
-def do(users):
-    for user in users.values():
-        print(user)
-do_xlsx("my xslx file.xlsx", headers, users)
