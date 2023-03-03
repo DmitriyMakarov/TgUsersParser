bdb76b50efaed8fdfa21c2eaed9e421485423331
dmakarov <i@dmitrijmakarov.ru>
2023-03-03 18:22:03 +0300

    ok

--- core/front_bot/bot.py
+++ core/front_bot/bot.py
@@ -3,29 +3,72 @@ from aiogram.dispatcher import Dispatcher
 import asyncio
 import configparser
 from core.front_bot import bot_menu
+from res.strings import bot_messages
+from aiogram.utils.helper import Helper, HelperMode, ListItem
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 
 config = configparser.ConfigParser()
 config.read('../../res/config/config.ini')
 config.sections()
 
+
 apy_key = config['TG']['api_key']
 print(apy_key)
 
 
 bot = Bot(token=apy_key)
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
 
-dp = Dispatcher(bot)
+
+class States(Helper):
+    mode = HelperMode.snake_case
+    LINK_STATE_0 = ListItem() #ждем ссылку
 
 @dp.message_handler(commands=["start"])
 async def cmd_start(message: types.Message):
     keyboard = bot_menu.send_welcome()
-    await message.answer("Hello!", reply_markup=keyboard)
+    await message.answer(bot_messages.start_message, reply_markup=keyboard)
 
+
+@dp.message_handler(state='*', commands=['getusers'])
+async def process_setstate_command(message: types.Message):
+    argument = message.get_args()
+    state = dp.current_state(user=message.from_user.id)
+    if not argument:
+        await state.reset_state()
+        return await message.reply('no argument')
+
+    if (not argument.isdigit()) or (not int(argument) < len(States.all())):
+        return await message.reply('key no'.format(key=argument))
+
+    await state.set_state(States.all()[int(argument)])
+    await message.reply('state change', reply=False)
+
+
+@dp.message_handler(state=States.LINK_STATE_0)
+async def first_test_state_case_met(message: types.Message):
+    await message.reply('Первый!', reply=False)
+    state = dp.current_state(user=message.from_user.id)
+    await state.reset_state()
+
+@dp.message_handler()
+async def cmd_start(message: types.Message):
+    print(message.text)
+    if message.text == 'Меню ℹ️':
+        keyboard = bot_menu.main_menu()
+        await message.answer('menu', reply_markup=keyboard)
 
 
 async def main():
     await dp.start_polling(bot)
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
 
 
 if __name__ == '__main__':
--- core/front_bot/bot_menu.py
+++ core/front_bot/bot_menu.py
@@ -1,9 +1,20 @@
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def send_welcome():
-    info_button = KeyboardButton('Info')
+    info_button = KeyboardButton('Меню ℹ️')
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(info_button)
 
     return keyboard
+
+def main_menu():
+    balance_inline_button = InlineKeyboardButton('Баланс 💵', callback_data='balance')
+    payment_inline_button = InlineKeyboardButton('Оплата 🛒', callback_data='payment')
+    help_inline_button = InlineKeyboardButton('Помощь 🆘', callback_data='help')
+    work_inline_button = InlineKeyboardButton('Спарсить пользователей ', switch_inline_query_current_chat='parse: ')
+    inline_keyboard = InlineKeyboardMarkup()
+    inline_keyboard.add(balance_inline_button, payment_inline_button)
+    inline_keyboard.add(help_inline_button)
+    inline_keyboard.add(work_inline_button)
+    return inline_keyboard
